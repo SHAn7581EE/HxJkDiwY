52ad73990a5c32e73eb77e7a9e9a030744339729
SHAn7581EE <hoodoos.thrifts-0r@icloud.com>
2025-08-13 21:48:49 +0900

    Create 网页内容抓取工具

---
+++ web_content_scraper_0813_2048_ajm.vue
@@ -0,0 +1,30 @@
+// 代码生成时间: 2025-08-13 20:48:48
+import axios from 'axios';
+
+export default {
+  name: 'WebContentScraper',
+  data() {
+    return {
+      url: '',
+      scrapeData: ''
+    };
+  },
+# 改进用户体验
+  methods: {
+    scrapeContent() {
+      this.scrapeData = '';  // 清空内容
+      axios.get(this.url)
+        .then(response => {
+          this.scrapeData = response.data;  // 响应式数据更新
+        }).catch(error => {
+          console.error('抓取失败:', error);
+        });
+    }
+  },
+# 添加错误处理
+  mounted() {
+    console.log('组件已挂载');
+  }
+# TODO: 优化性能
+};
+</script>
