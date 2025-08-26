632af26c654a07612ba5a3d6602747f8a0dff3f4
SHAn7581EE <hoodoos.thrifts-0r@icloud.com>
2025-08-26 12:36:37 +0900

    Implement 排序算法实现 using VUE

---
+++ sorting_algorithm_component_0826_1136_rtx.vue
@@ -0,0 +1,46 @@
+// 代码生成时间: 2025-08-26 11:36:36
+import { ref, onMounted } from 'vue';
+
+export default {
+  name: 'SortingAlgorithmComponent',
+  setup() {
+    // 响应式数据
+    const numbers = ref([5, 2, 9, 1, 5, 6]);
+    const sortedArray = ref([]);
+
+    // 冒泡排序算法
+    const bubbleSort = (array) => {
+      for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - i - 1; j++) {
+          if (array[j] > array[j + 1]) {
+            const temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+          }
+        }
+      }
+      return array;
+    };
+
+    // 排序函数
+    const sortArray = () => {
+      sortedArray.value = bubbleSort([...numbers.value]);
+    };
+
+    // 生命周期钩子
+    onMounted(() => {
+      console.log('Component is mounted.');
+    });
+
+    return {
+      numbers,
+      sortedArray,
+      sortArray
+    };
+  }
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
