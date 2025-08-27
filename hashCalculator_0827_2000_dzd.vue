2457cbb1361b723c40a49b56bdf288edad1669d9
SHAn7581EE <hoodoos.thrifts-0r@icloud.com>
2025-08-27 21:00:25 +0900

    Implement 哈希值计算工具 using VUE

---
+++ hashCalculator_0827_2000_dzd.vue
@@ -0,0 +1,26 @@
+// 代码生成时间: 2025-08-27 20:00:24
+import { ref, onMounted } from 'vue';
+import CryptoJS from 'crypto-js';
+
+// Reactive data
+const inputText = ref('');
+const hashValue = ref('');
+
+// Lifecycle hook
+onMounted(() => {
+  console.log('Hash Calculator component is mounted');
+});
+
+// Function to calculate hash
+function calculateHash() {
+  if (inputText.value) {
+    hashValue.value = CryptoJS.SHA256(inputText.value).toString();
+  } else {
+    hashValue.value = '';
+  }
+}
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
