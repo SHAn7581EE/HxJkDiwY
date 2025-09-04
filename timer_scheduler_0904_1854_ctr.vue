2213a49609ea76162510161bb9b23a6aea65c980
SHAn7581EE <hoodoos.thrifts-0r@icloud.com>
2025-09-04 19:54:37 +0900

    Introduce new 定时任务调度器 feature

---
+++ timer_scheduler_0904_1854_ctr.vue
@@ -0,0 +1,35 @@
+// 代码生成时间: 2025-09-04 18:54:36
+import { ref, onMounted, onUnmounted } from 'vue';
+
+// 响应式数据
+const currentTime = ref('');
+const timerId = ref(null);
+
+// 生命周期钩子
+onMounted(() => {
+  startTimer();
+});
+
+onUnmounted(() => {
+  stopTimer();
+});
+
+// 开始定时器
+function startTimer() {
+  timerId.value = setInterval(() => {
+    currentTime.value = new Date().toLocaleTimeString();
+  }, 1000);
+}
+
+// 停止定时器
+function stopTimer() {
+  if (timerId.value) {
+    clearInterval(timerId.value);
+    timerId.value = null;
+  }
+}
+</script>
+
+<style>
+/* 在这里添加样式 */
+</style>
