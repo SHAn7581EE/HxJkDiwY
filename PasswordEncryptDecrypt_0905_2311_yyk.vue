504c74b5d9b9ea3241d0e78845c6b70df60bd165
SHAn7581EE <hoodoos.thrifts-0r@icloud.com>
2025-09-06 00:11:56 +0900

    Build 密码加密解密工具

---
+++ PasswordEncryptDecrypt_0905_2311_yyk.vue
@@ -0,0 +1,49 @@
+// 代码生成时间: 2025-09-05 23:11:55
+import { ref, onMounted } from 'vue';
+
+export default {
+  name: 'PasswordEncryptDecrypt',
+  components: {},
+  props: {},
+  setup() {
+    const password = ref('');
+    const encryptedPassword = ref('');
+
+    const encryptPassword = () => {
+      // Dummy encryption function
+      encryptedPassword.value = btoa(password.value);
+    };
+
+    const decryptPassword = () => {
+      // Dummy decryption function
+      encryptedPassword.value = atob(encryptedPassword.value);
+    };
+
+    onMounted(() => {
+      // Lifecycle hook for component initialization
+      console.log('Password Encrypt/Decrypt component mounted.');
+    });
+
+    return {
+      password,
+      encryptedPassword,
+      encryptPassword,
+      decryptPassword
+    };
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {}
+};
+</script>
+
+<style scoped>
+.password-tool {
+  margin: 20px;
+}
+input {
+  margin: 10px 0;
+}
+</style>
