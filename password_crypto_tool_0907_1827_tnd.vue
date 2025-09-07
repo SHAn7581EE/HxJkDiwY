f2298e3abf9b09aff5c490fc23108d6056d6b77c
SHAn7581EE <hoodoos.thrifts-0r@icloud.com>
2025-09-07 19:27:46 +0900

    Develop JS 密码加密解密工具

---
+++ password_crypto_tool_0907_1827_tnd.vue
@@ -0,0 +1,34 @@
+// 代码生成时间: 2025-09-07 18:27:46
+import { ref, onMounted } from 'vue';
+
+// 响应式数据
+const password = ref('');
+const encryptedPassword = ref('');
+const isEncrypted = ref(false);
+
+// 加密解密函数
+function toggleCrypto() {
+  if (isEncrypted.value) {
+    // 解密
+    encryptedPassword.value = atob(password.value);
+  } else {
+    // 加密
+    encryptedPassword.value = btoa(password.value);
+  }
+  isEncrypted.value = !isEncrypted.value;
+}
+
+// 生命周期钩子
+onMounted(() => {
+  console.log('Password Crypto Tool component is mounted.');
+});
+</script>
+
+<style scoped>
+.password-crypto-tool input {
+  margin-right: 10px;
+}
+.password-crypto-tool button {
+  cursor: pointer;
+}
+</style>
