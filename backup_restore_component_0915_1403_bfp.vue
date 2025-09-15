e0442370070acd244d18b6499c2ef2ba8e3f4161
SHAn7581EE <hoodoos.thrifts-0r@icloud.com>
2025-09-15 15:03:04 +0900

    Add 数据备份恢复

---
+++ backup_restore_component_0915_1403_bfp.vue
@@ -0,0 +1,39 @@
+// 代码生成时间: 2025-09-15 14:03:04
+import { ref, onMounted, onUnmounted } from 'vue';
+
+export default {
+  name: 'BackupRestoreComponent',
+
+  data() {
+    return {
+      data: ref(''),
+      backupData: null,
+    };
+  },
+
+  methods: {
+    async backupData() {
+      this.backupData = JSON.stringify(this.data);
+      console.log('Data backed up:', this.backupData);
+    },
+
+    async restoreData() {
+      if (this.backupData) {
+        this.data = JSON.parse(this.backupData);
+        console.log('Data restored:', this.data);
+      } else {
+        alert('No backup data available to restore.');
+      }
+    },
+  },
+
+  mounted() {
+    this.data = 'Initial Data';
+    console.log('Component mounted and data initialized.');
+  },
+
+  unmounted() {
+    console.log('Component unmounted.');
+  },
+};
+</script>
